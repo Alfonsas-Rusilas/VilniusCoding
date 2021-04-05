4334376e927a6621085ba94b22e7473343d247bc
Alfonsas-Rusilas <alfonsas.rusilas@gmail.com>
2021-04-05 21:30:22 +0300

    007 Duonos kepimas

---
+++ HomeWork3/HomeWork3/Salygu_uzduotys/007_duonos_kepimas.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork3.Salygu_uzduotys
+{
+    class _007_duonos_kepimas
+    {
+        static void Main(string[] args)
+        {
+            int valandos = 8;
+            Console.Write("Kiek iškepama duonos kepalų per valandą: ");
+            int kepalai = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Įveskite kiek darbuotojų dirba: ");
+            int darbuotojai = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Įveskite vieno kepalo savikainą: ");
+            double savikaina = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Įveskite vieno kepalo pardavimo kainą: ");
+            double kaina = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine($"Per dieną iškepama {kepalai * valandos} kepalų duonos.");
+            double dienos_savikaina = kepalai * valandos * savikaina;
+            double dienos_pajamos = kepalai * valandos * kaina;
+            Console.WriteLine($"Iškeptos duonos savikaina: {dienos_savikaina}, pajamos {dienos_pajamos}, pelnas {dienos_pajamos-dienos_savikaina};");
+        }
+    }
+}
