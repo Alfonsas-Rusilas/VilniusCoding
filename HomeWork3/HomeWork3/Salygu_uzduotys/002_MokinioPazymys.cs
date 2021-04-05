69bb8316cd17a9e4151c5f6056b716bd08bb2c28
Alfonsas-Rusilas <alfonsas.rusilas@gmail.com>
2021-04-05 20:20:26 +0300

    002 Mokinio pažymys

--- HomeWork3/HomeWork3/Salygu_uzduotys/002_MokinioPazymys.cs
+++ HomeWork3/HomeWork3/Salygu_uzduotys/002_MokinioPazymys.cs
@@ -8,6 +8,40 @@ namespace HomeWork3.Salygu_uzduotys
 {
     class _002_MokinioPazymys
     {
+        static void Main(string[] args)
+        {
+            Console.Write("Įveskite mokinio pažymį: ");
+            int paz = Convert.ToInt32(Console.ReadLine());
 
+            if (paz < 1 || paz > 10)
+            {
+                Console.WriteLine("Klaida, neteisingas pažymys.");
+            }
+            else if (paz == 10)
+            {
+                Console.WriteLine("Puiku");
+            }
+            else if (paz == 9 || paz == 8)
+            {
+                Console.WriteLine("Labai gerai");
+            }
+            else if (paz == 7 || paz == 6)
+            {
+                Console.WriteLine("Gerai");
+            }
+            else if (paz == 5)
+            {
+                Console.WriteLine("Vidutiniškai");
+            }
+            else if (paz == 4)
+            {
+                Console.WriteLine("Bent teigiamas");
+            }
+            else 
+            {
+                Console.WriteLine("Labai blogai");
+            }
+
+        }
     }
 }
