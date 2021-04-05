69e76772ce42196c84192b7852639d36639229d2
Alfonsas-Rusilas <alfonsas.rusilas@gmail.com>
2021-04-05 20:13:29 +0300

    Pirmas - ar turi 18 metų?

---
+++ HomeWork3/HomeWork3/Salygu_uzduotys/001_ArTuri18.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace HomeWork3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Įveskite amžių: ");
+            int amzius = Convert.ToInt32(Console.ReadLine());
+
+            if (amzius >= 18)
+            {
+                Console.WriteLine("Turi bent 18 metų.");
+            }
+            else
+            {
+                Console.WriteLine("Neturi 18 metų.");
+            }
+
+            if (amzius % 2 == 0)
+            {
+                Console.WriteLine("Amžiaus skaičius lyginis");
+            }
+            else
+            {
+                Console.WriteLine("Amžiaus skaičius nelyginis.");
+            }
+
+        }
+    }
+}
---
+++ HomeWork3/HomeWork3/Salygu_uzduotys/002_MokinioPazymys.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork3.Salygu_uzduotys
+{
+    class _002_MokinioPazymys
+    {
+
+    }
+}
