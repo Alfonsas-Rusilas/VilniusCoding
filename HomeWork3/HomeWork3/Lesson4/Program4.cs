182413c2f7fd4d1c7711c5b2f8fb7595ab843f29
Alfonsas-Rusilas <alfonsas.rusilas@gmail.com>
2021-04-07 19:11:40 +0300

    3.a užduotys

--- HomeWork3/HomeWork3/Lesson4/Program4.cs
+++ HomeWork3/HomeWork3/Lesson4/Program4.cs
@@ -8,13 +8,38 @@ namespace HomeWork3.Lesson4
 {
     class Program4
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            // i, j, k, l, m, n
-            for (int i = 0; i < 5; i++)
+            //////////////  3.a
+            for (int i = 0; i < 20; i += 3)
             {
-                Console.WriteLine("OK");
-            } 
+                Console.WriteLine($"Hi! i is: {i}");
+            }
+
+
+            Console.Write("Įveskite pirmą skaičių: ");
+            int skaicius1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Įveskite antrą skaičių: ");
+            int skaicius2 = Convert.ToInt32(Console.ReadLine());
+
+            if (skaicius1 > skaicius2)
+            {
+                Console.WriteLine("bad limits");
+            }
+            else
+            {
+                for (int i = skaicius1; i < skaicius2; i++)
+                {
+                    Console.WriteLine($"Ciklas: {i}, kvadratas: {i * i}");
+                }
+            }
+
+            int suma = 0;
+            for (int i = 1000; i > 0; i--)
+            {
+                if ((i % 3) == 0 || (i % 5 == 0)) suma += i;
+            }
+            Console.WriteLine($"Hi! suma is: {suma}");
         }
     }
 }
