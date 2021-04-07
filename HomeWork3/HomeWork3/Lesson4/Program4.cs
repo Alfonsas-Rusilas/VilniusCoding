f7e690dccdd3f38acc34e3a1aa0d4601587ecdd0
Alfonsas-Rusilas <alfonsas.rusilas@gmail.com>
2021-04-07 19:42:35 +0300

    3.b užduotis

--- HomeWork3/HomeWork3/Lesson4/Program4.cs
+++ HomeWork3/HomeWork3/Lesson4/Program4.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace HomeWork3.Lesson4
 {
@@ -10,19 +6,19 @@ namespace HomeWork3.Lesson4
     {
         static void Main()
         {
-            //////////////  3.a
+            //////////////  3.a /////////////////////////////////////////
+            /*
             for (int i = 0; i < 20; i += 3)
             {
                 Console.WriteLine($"Hi! i is: {i}");
             }
-
 
             Console.Write("Įveskite pirmą skaičių: ");
             int skaicius1 = Convert.ToInt32(Console.ReadLine());
             Console.Write("Įveskite antrą skaičių: ");
             int skaicius2 = Convert.ToInt32(Console.ReadLine());
 
-            if (skaicius1 > skaicius2)
+            if (skaicius1 >= skaicius2)
             {
                 Console.WriteLine("bad limits");
             }
@@ -39,7 +35,25 @@ namespace HomeWork3.Lesson4
             {
                 if ((i % 3) == 0 || (i % 5 == 0)) suma += i;
             }
-            Console.WriteLine($"Hi! suma is: {suma}");
+            Console.WriteLine($"\nHi! suma skaičių, mažesnių už 1000 ir kurie dalinasi iš 3 arba 5: {suma}");
+            */
+            //////////////  END of 3.a /////////////////////////////////////////
+            // Ctrl + K,  Ctrl + C
+
+
+            ////////////// 3.b /////////////////////////////////////////
+
+            Console.WriteLine("Pasirinkite vieną:\n1 kava;\n2 arbata;\n3 vanduo");
+            int meniu = Convert.ToInt32(Console.ReadLine());
+            while (meniu < 1 || meniu > 3)
+            {
+                Console.WriteLine("Klaida!");
+                Console.Write("Įveskite tinkamą skaičių: ");
+                meniu = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine($"Pasirinkote: {meniu}");
+
+
         }
     }
 }
