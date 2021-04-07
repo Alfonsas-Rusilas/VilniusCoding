1a51c74fb837f589cd0c80e2683005a3fc9fcebe
Alfonsas-Rusilas <alfonsas.rusilas@gmail.com>
2021-04-07 18:52:18 +0300

    Lesson4 - starting

---
+++ HomeWork3/HomeWork3/Lesson4/Program4.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork3.Lesson4
+{
+    class Program4
+    {
+        static void Main(string[] args)
+        {
+            // i, j, k, l, m, n
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("OK");
+            } 
+        }
+    }
+}
---
+++ HomeWork3/HomeWork3/Pamoka_3/pamoka_3.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork3.Pamoka_3
+{
+    class pamoka_3
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Įveskite pirmą skaičių: ");
+            int a = 5;
+
+            switch (a)
+            {
+                case 1:
+                    Console.WriteLine("Case 1");
+                    break;
+                case 2:
+                    Console.WriteLine("Case 2");
+                    break;
+                case 5:
+                case 6:
+                    Console.WriteLine("Case 5 ir 6");
+                    break;
+                default:
+                    Console.WriteLine("Case default");
+                    break;
+            }
+        }
+    }
+}
