1a935deffdc613ca6a3c2fda136e8082e8b9f8a1
Alfonsas-Rusilas <alfonsas.rusilas@gmail.com>
2021-04-08 19:42:56 +0300

    Exercise1()

--- HomeWork5/HomeWork5/Lesson5/Program5.cs
+++ HomeWork5/HomeWork5/Lesson5/Program5.cs
@@ -11,12 +11,30 @@ namespace HomeWork5.Lesson5
         // Metodai - tai konkrečios klasės metodas
         // Jei main yra static - tai funkcijos už jos taip pat "static"
         // static or public
+        // static void MyMethod(string country = "Norway") 
+        // static void MyMethod(string fname, int age) 
+        //
+        // static void MyMethod(string child1, string child2, string child3) 
+        // MyMethod(child3: "John", child1: "Liam", child2: "Liam"); 
+        //
+        // static void MyMethod(string child1 = "Liam", string child2 = "Jenny", string child3 = "John")
         // 
+        // int MyMethod(int x)
+        // float MyMethod(float x)
+        // double MyMethod(double x, double y)
+        //
+        // DoSom(leftParam:5, rightParam:84) - nurodome parametrus pagal vardą
+        //
+        // Ctrl + . //// sukuria karkasą pagal pavadinimą (arba Alt + Shift + F10)
+
+        //////////////////// Metodai ir Funkcijos ////////////////////////
+        /*
         static void Main()
         {
             Console.WriteLine("==============================");
             SayHello();
-
+            //SaySomething();
+          
         }
 
         static void SayHello()
@@ -34,6 +52,53 @@ namespace HomeWork5.Lesson5
         static void SayBay()
         {
             Console.WriteLine("SayBay...");
+        }
+        */
+        //////////////// 5.a /////////////////////////////////
+
+        static void Main()
+        {
+            Console.WriteLine("======================== 5.a ===========================");
+            Exercise1();
+        }
+
+        static void Exercise1()
+        {
+            Console.Write("Įveskite pirmą skaičių: ");
+            int sk1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Įveskite antrą skaičių: ");
+            int sk2 = Convert.ToInt32(Console.ReadLine());
+
+            Suma(sk1,sk2);
+            Skirtumas(sk1, sk2);
+            Sandauga(sk1, sk2);
+            Dalmuo(sk1, sk2);
+            
+            Suma();
+            Skirtumas();
+            Sandauga();
+            Dalmuo();
+        }
+
+        static void Dalmuo(int sk1 = 2, int sk2 = 3)
+        {
+            Console.WriteLine($"Dalmuo: {sk1} ir {sk2}");
+        }
+
+        static void Sandauga(int sk1 = 2, int sk2 = 3)
+        {
+            Console.WriteLine($"Sandauga: {sk1} ir {sk2}");
+        }
+
+        static void Skirtumas(int sk1 = 2, int sk2 = 3)
+        {
+            Console.WriteLine($"Skirtumas: {sk1} ir {sk2}");
+        }
+
+        static void Suma(int sk1 = 5, int sk2 = 10)
+        {
+            Console.WriteLine($"Suma: {sk1} ir {sk2}");
         }
     }
 }
