5632f8e8b6bc50d701cf2858392e717b97795ad2
Alfonsas-Rusilas <alfonsas.rusilas@gmail.com>
2021-04-08 18:53:20 +0300

    Start HomeWork5 + Lesson5

---
+++ HomeWork5/HomeWork5/Lesson5/Program5.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork5.Lesson5
+{
+    class Program5
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello World!");
+            DoSomething();
+        }
+
+        static void DoSomething()
+        {
+            int data = 5;
+            Console.WriteLine(data);
+        }
+    }
+}
