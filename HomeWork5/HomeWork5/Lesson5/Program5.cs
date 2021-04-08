4fa13bbcd43eb6961f0a712da0d31c94007d0d77
Alfonsas-Rusilas <alfonsas.rusilas@gmail.com>
2021-04-08 19:03:17 +0300

    SayHello metodai

--- HomeWork5/HomeWork5/Lesson5/Program5.cs
+++ HomeWork5/HomeWork5/Lesson5/Program5.cs
@@ -2,22 +2,38 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace HomeWork5.Lesson5
 {
     class Program5
     {
+        // Funkcijos - iš didžiosios (ar didžiųjų). Standartiškai aprašomai atskirai;
+        // Metodai - tai konkrečios klasės metodas
+        // Jei main yra static - tai funkcijos už jos taip pat "static"
+        // static or public
+        // 
         static void Main()
         {
-            Console.WriteLine("Hello World!");
-            DoSomething();
+            Console.WriteLine("==============================");
+            SayHello();
+
         }
 
-        static void DoSomething()
+        static void SayHello()
         {
-            int data = 5;
-            Console.WriteLine(data);
+            Console.WriteLine("Hello!");
+            SaySomething();
+        }
+
+        static void SaySomething()
+        {
+            Console.WriteLine("SaySomething!");
+            SayBay();
+        }
+
+        static void SayBay()
+        {
+            Console.WriteLine("SayBay...");
         }
     }
 }
