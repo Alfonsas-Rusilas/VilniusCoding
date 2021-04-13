a8eeedcb2c8d3e59bb7ccb379493fd29533f073d
Alfonsas-Rusilas <alfonsas.rusilas@gmail.com>
2021-04-13 22:02:03 +0300

    6c - params objektai

--- HomeWork6/HomeWork6/Lesson6/Lesson6c.cs
+++ HomeWork6/HomeWork6/Lesson6/Lesson6c.cs
@@ -11,6 +11,25 @@ namespace HomeWork6.Lesson6
         static void Main(string[] args)
         {
             //
+            ParamsTesting(44, new double[] { 1.2, 2.2, 3.2}, "pirmas", "antras", "trečias");
+            //
+        }
+
+        static void ParamsTesting(int i, double[] db, params string[] rest)
+        {
+            Console.WriteLine($"Parametas 1: {i}");
+            string temp = "";
+            foreach (var item in db)
+            {
+                temp += item + " ";
+            }
+            Console.WriteLine($"Parametas 2: {temp}");
+            temp = "";
+            foreach (var item in rest)
+            {
+                temp += item + " ";
+            }
+            Console.WriteLine($"Parametas 3: {temp}");
         }
 
         static string PrintArray(string[] str_array, string delimiter = " ")
