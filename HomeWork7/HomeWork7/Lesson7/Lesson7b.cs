5da9f36a1edf7397d047701105d8a0565e5627c5
Alfonsas-Rusilas <alfonsas.rusilas@gmail.com>
2021-04-14 20:45:19 +0300

    7b - starting

--- HomeWork7/HomeWork7/Lesson7/Lesson7b.cs
+++ HomeWork7/HomeWork7/Lesson7/Lesson7b.cs
@@ -10,7 +10,76 @@ namespace HomeWork7.Lesson7
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            /////////////////////// 7b   - collections
+
+            List<int> main_list = new List<int>() { -1, -2, -3, -4, -5 };
+            List<int> sub_list = new List<int>() { 10, 20, 30, 40, 50, 60 };
+
+            main_list.Add(9);
+            main_list.Insert(0, 0);
+            PrintValues(main_list);
+            Console.WriteLine($"Elementų: {main_list.Count}");
+
+            Console.WriteLine($"Conatains 20?: : {main_list.Contains(20)}");
+
+            Console.WriteLine($"IndexOf 4 = {main_list.IndexOf(4)}");
+
+            Console.WriteLine($"Find(x>0): {main_list.Find(isPositive)}");
+
+            Console.WriteLine($"main_list.Exists(isPositive): {main_list.Exists(isPositive)}");
+
+            Console.WriteLine($"main_list.TrueForAll(isPositive): {main_list.TrueForAll(isPositive)}");
+
+            main_list.Remove(60);
+            main_list.RemoveAt(2);
+            main_list.RemoveRange(4, 2);
+            PrintValues(main_list);
+
+            main_list.Clear();
+
+            main_list.InsertRange(0, sub_list);
+            PrintValues(main_list);
+
+            main_list.Sort();
+            PrintValues(main_list);
+
+            main_list.RemoveAll(FindEven);
+            PrintValues(main_list);
+
+            //var teigiami = new Predicate<int>(isPositive);
+            //var filtered = main_list.FindAll(teigiami);
+            //var filtered = main_list.FindAll(isPositive);
+
+
+            //PrintValues(filtered);
+
+            //main_list.RemoveAll(FindEven);
+            //PrintValues(main_list);
+        }
+
+
+        static bool isPositive(int val)
+        {
+            return val > 0;
+        }
+
+        static bool FindEven(int val)
+        {
+            return val % 2 == 0;
+        }
+
+        static void PrintValues(List<int> myList)
+        {
+            foreach (int i in myList)
+                Console.Write($"   {i}");
+            Console.WriteLine();
+        }
+
+        static void PrintValues(List<string> myList)
+        {
+            foreach (string i in myList)
+                Console.Write($"   {i}");
+            Console.WriteLine();
         }
     }
 }
