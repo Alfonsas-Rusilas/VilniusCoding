f1e24c210bc61ece06c566df60632775699d5600
Alfonsas-Rusilas <alfonsas.rusilas@gmail.com>
2021-04-28 21:40:14 +0300

    + null able numbers

---
+++ BigHomeWork3/BigHomeWork3/Lesson13/Null/NullClass.cs
@@ -0,0 +1,30 @@
+﻿using BigHomeWork3.GameSample.Unit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BigHomeWork3.Lesson13.Null
+{
+    class NullClass
+    {
+        public static void Main()
+        {
+            // int s = null;   // error!
+
+            int? nulable_int = null; // tai įpakuotas skaičius į objektą, ir šis objektas gali nurodyti null ar bet kokią kitą reikšmę
+
+            Hero h = new Hero("name",1,5);
+            h = null;
+
+            if (h == null)
+            {
+                // tuščias objektas, veiksmų negalime atlikti!
+            }
+
+
+        }
+    }
+
+}
