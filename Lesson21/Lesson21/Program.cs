0359618602a3c2fe69e9e401c82c2ce87be01818
Alfonsas-Rusilas <alfonsas.rusilas@gmail.com>
2021-05-17 19:14:52 +0300

    Singleton class - start

---
+++ Lesson21/Lesson21/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Lesson21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton singleton1 = Singleton.Instance;
+            Singleton singleton2 = Singleton.Instance;
+
+            if (singleton1 == singleton2 && singleton1 != null)
+            {
+                Console.WriteLine("Viksas gerai!");
+            } else
+            {
+                Console.WriteLine("Blogai, skirtingi objektai arba tušti");
+            }
+
+
+
+            //Console.WriteLine("Hello World!");
+        }
+    }
+}
